1810edd3b8814f39da19b8622fac82068f90cde1
mohamedayari1 <ayarim781@gmail.com>
2026-04-28 12:15:57 +0100

    update layout

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
 
         <footer className="bg-blue-50 py-12">
           <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Made with 💗 by RoadsideCoder</p>
+            <p>Made with 💗 by Mohamed Ayari</p>
           </div>
         </footer>
       </body>
